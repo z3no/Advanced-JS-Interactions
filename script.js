5a44e2725d6e83577cb77df75f2a7d708657e6c8
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-30 14:54:02 +0200

    light/dark mode

--- script.js
+++ script.js
@@ -15,16 +15,72 @@ function randomLetter(){
  */
 
 //add the images to the parallax carousel
-let carouselImages = [document.querySelectorAll('.carouselImage')];
+let carouselImages = [...document.querySelectorAll('.carouselImage')];
 
 carouselImages.forEach((image, index) => {
-    carouselImages.style.backgroundImage = `url(images/${index+1}.jpg)`;
+    image.style.backgroundImage = `url(images/${index+1}.jpg)`;
 })
 
 // Parallax carousel
-let carousel = document.querySelector('.carouselContainer');
+let carousel = document.querySelector('.slide');
 let carouselWidth;
 let carouselImagesWidth;
 let current = 0;
 let target = 0;
 let ease = 0.05;
+
+// Linear interpolation function
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+}
+
+// Transform function
+function setTransform (el, transform) {
+    el.style.transform = transform;
+}
+
+// Initializing function
+function init () {
+    carouselWidth = carousel.getBoundingClientRect().width;
+    carouselImagesWidth = carouselWidth/carouselImages.length;
+    document.body.style.height = `${carouselWidth - (window.innerWidth - window.innerHeight)}px`;
+}
+
+// Animating the carousel
+function animate() {
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    target = window.scrollY;
+    setTransform(carousel, `translateX(${-current}px)`)
+    animateImages();
+    requestAnimationFrame(animate);
+}
+
+// Animate images for parallax effect
+function animateImages() {
+    let ratio = current/carouselImagesWidth;
+    let intersectionRatioValue;
+    carouselImages.forEach((image, index) => {
+        intersectionRatioValue = ratio - (index * 0.7);
+        setTransform(image, `translateX(${intersectionRatioValue * 70}px)`);
+    });
+}
+
+init();
+animate();
+
+/*
+ *
+ *      CHASING CIRCLE
+ *
+ */
+
+
+/*
+ *
+ *      LIGHT MODE / DARK MODE
+ *
+ */
+let darkModeLight = document.querySelector('.mode');
+darkModeLight.addEventListener('click', function (){
+    document.body.classList.toggle('darkMode');
+})
